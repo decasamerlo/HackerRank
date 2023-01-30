2e65fcae660744c2aeace571455f46cf4719c413
Andre <casagrandamerlo@gmail.com>
2023-01-30 15:10:29 -0300

    001 Added Javascript solution

---
+++ 001 - Plus Minus/Solution.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let counter = new Array(3).fill(0);
+
+    for (let value of arr) {
+        if (value > 0) {
+            counter[0]++;
+        } else if (value < 0) {
+            counter[1]++;
+        } else {
+            counter[2]++;
+        }
+    }
+
+    counter.forEach(value => console.log((value/arr.length).toFixed(6)));
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
